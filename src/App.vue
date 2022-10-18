e41db5b09bdd3e16747de526774735d0f85bf741
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-18 12:49:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!-- START TEMPLATE -->
+<template>
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+console.log(supabase);
+
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<!-- START TEMPLATE -->
+<template>
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
