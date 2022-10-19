aec1a2c9d9510f7e38079ad5a50362754845dad9
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-19 19:01:41 +0200

    added footer and navbar components

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<!-- START TEMPLATE -->
+<template>
+
+    <div class="sector">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-12">
+                    Copyright
+                </div>
+            </div>
+        
+        </div>
+    </div>
+
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<!-- START TEMPLATE -->
+<template>
+
+    <div class="section">
+        <div class="container">
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                        <a>Home</a>
+
+                        <!-- <RouterLink class="navbar-item" :to="{name: 'home'}">
+                            Home
+                        </RouterLink>
+                        <RouterLink class="navbar-item" :to="{name: 'login'}">
+                            Login
+                        </RouterLink> -->
+                </div>
+            </nav>
+        </div>
+    </div>
+    
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
