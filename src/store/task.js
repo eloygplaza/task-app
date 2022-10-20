fa9aaa5080b45f468e868c8425b77cb85dacb530
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-20 17:56:45 +0200

    iserTask and MarkAsDone actions added

---
+++ src/store/task.js
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia";
+
+export const taskStore = defineStore("taskStore", {
+  state: () => ({
+    tasks: [],
+  }),
+  persist: [
+    {
+      paths: ["tasks"],
+      storage: sessionStorage,
+    },
+  ],
+  actions: {
+    insertTask(userId, title, description) {
+      this.tasks.data.unshift({
+        id: this.tasks.data.at(0).id + 1,
+        created_at: Date.now,
+        user_id: userId,
+        title: title,
+        description: description,
+        done: false,
+      });
+    },
+    markAsDone(taskId) {
+      this.tasks.data.find((el) => {
+        if (el.id == taskId) {
+          el.done = !el.done;
+        }
+      });
+    },
+  },
+});
